c640549bb7b2814ed1a85edca39c55100b6f8ef9
Prashant Sahni <prashant.sahni5@gmail.com>
2014-10-21 15:45:13 +0530

    working on blog application

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from blog.models import Post
 
 # Create your views here.
+
+
+def index(request):
+    # Get the blog posts that are published
+    posts = Post.objects.filter(published=True)
+    #Now return the rendered template
+    return render(request, 'blog/index.html', {'posts': posts})
+
+
+
+def post(request, slug):
+    # Get the Post object
